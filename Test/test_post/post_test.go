a0dce4592ffaeefac9cc1381d6d2cb8bd280704a
PratigyaPatidar145 <109739231+PratigyaPatidar145@users.noreply.github.com>
2023-06-20 00:34:48 +0530

    changes in test file test functions

--- Test/test_post/post_test.go
+++ Test/test_post/post_test.go
@@ -1,82 +1,178 @@
-package main
+package validation_test
 
 import (
-	"testing"
-
 	Register "boilerplate/model/register"
 	valid "boilerplate/util/Validation"
+	"testing"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateUser(t *testing.T) {
-	tests := []struct {
-		name    string
-		request Register.Register
-		want    bool
-	}{
-		{
-			name: "Valid username",
-			request: Register.Register{
-				Username: "john.doe",
-			},
-			want: true,
+type test struct {
+	name    string
+	request Register.Register
+	want    bool
+}
+
+func TestRegistration_form_username(t *testing.T) {
+	testcase := test{
+		name: "ValidRegistration_user",
+		request: Register.Register{
+			Username:  "Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "123456789012",
+			Mobile:    "6158911234",
+			Password:  "Prat@235687",
 		},
-		{
-			name: "Invalid username",
-			request: Register.Register{
-				Username: "john$doe",
-			},
-			want: false,
-		},
-		{
-			name: "Valid password",
-			request: Register.Register{
-				Password: "Abc123!",
-			},
-			want: true,
-		},
-		{
-			name: "Invalid password",
-			request: Register.Register{
-				Password: "password123",
-			},
-			want: false,
-		},
-		{
-			name: "Valid PAN and Aadhaar",
-			request: Register.Register{
-				Pan:   "ABCDE1234F",
-				Adhar: "123456789012",
-			},
-			want: true,
-		},
-		{
-			name: "Invalid PAN and Aadhaar",
-			request: Register.Register{
-				Pan:   "ABCDE1234G",
-				Adhar: "12345678901",
-			},
-			want: false,
-		},
-		{
-			name: "Invalid PAN and empty Aadhaar",
-			request: Register.Register{
-				Pan:   "ABCDE1234G",
-				Adhar: "",
-			},
-			want: false,
-		},
+		want: true,
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			v := validator.New()
-			v.RegisterValidation("UserValidation", valid.ValidateUser)
-			err := v.Struct(test.request)
-			got := (err == nil)
-			assert.Equal(t, test.want, got)
-		})
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got)
+	})
+
+}
+
+func TestRegistration_form_invalid_username(t *testing.T) {
+	testcase := test{
+		name: "In_validRegistration_user",
+		request: Register.Register{
+			Username:  "_Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "123456789012",
+			Mobile:    "6158911234",
+			Password:  "Prat@235687",
+		},
+		want: false,
 	}
+
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got, "Validation result does not match for test: %s", testcase.name)
+	})
+
+}
+
+func TestRegistration_form_pan_adhar(t *testing.T) {
+	testcase := test{
+		name: "ValidRegistration_pan_adhar",
+		request: Register.Register{
+			Username:  "Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "123456789012",
+			Mobile:    "6158911234",
+			Password:  "Prat@235687",
+		},
+		want: true,
+	}
+
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got)
+	})
+
+}
+
+func TestRegistration_form_invalid_pan_adhar(t *testing.T) {
+	testcase := test{
+		name: "In_validRegistration_pan_adhar",
+		request: Register.Register{
+			Username:  "_Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "qwer68-098-0",
+			Mobile:    "6158911234",
+			Password:  "Prat@235687",
+		},
+		want: false,
+	}
+
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got, "Validation result does not match for pan or adhar test: %s", testcase.name)
+	})
+
+}
+
+func TestRegistration_form_password(t *testing.T) {
+	testcase := test{
+		name: "ValidRegistration_password",
+		request: Register.Register{
+			Username:  "Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "123456789012",
+			Mobile:    "6158911234",
+			Password:  "Prat@235687",
+		},
+		want: true,
+	}
+
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got, "Validation result does not match for password test: %s", testcase.name)
+	})
+
+}
+
+func TestRegistration_form_invalid_password(t *testing.T) {
+	testcase := test{
+		name: "In_validRegistration_password",
+		request: Register.Register{
+			Username:  "Pratigya",
+			Firstname: "pratigya",
+			Lastname:  "123456",
+			Email:     "vivek@gmail.com",
+			Pan:       "",
+			Adhar:     "123456789012",
+			Mobile:    "6158911234",
+			Password:  "Prat235687",
+		},
+		want: false,
+	}
+
+	t.Run(testcase.name, func(t *testing.T) {
+		v := validator.New()
+		v.RegisterValidation("UserValidation", valid.ValidateUser)
+		err := v.Struct(testcase.request)
+
+		got := (err == nil)
+		assert.Equal(t, testcase.want, got, "Validation result does not match for password test: %s", testcase.name)
+	})
+
 }
